1a1445c66604c0fe2b85fe1ac872ca31615b10f8
AlexMato <matontialessandro0@gmail.com>
2020-04-07 18:14:21 +0200

    definizione dettagli

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   $("#invia").click(
+     function(){
+       var messaggio = $("#mex").val();
+       var asd = $("#prova");
+       asd.append(messaggio);
+     }
+   )
+
+
+});
